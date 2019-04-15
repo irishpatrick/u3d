8c8408537f9156e047ababe73c6ec7599f202364
Patrick Roche <rochep63@gmail.com>
2019-04-15 14:04:27 -0400

    latest

--- u3d/src/Shader.cpp
+++ u3d/src/Shader.cpp
@@ -1,1 +1,23 @@
 #include "Shader.hpp"
+#include <cstdio>
+#include <cstdlib>
+#include <GL/glew.h>
+
+namespace u3d
+{    
+    void Shader::compile(const std::string& vertexFile, const std::string& fragmentFile)
+    {
+        FILE* fp;
+
+        fp = fopen(vertexFile.c_str(), "rb");
+        fseek(fp, 0L, SEEK_END);
+        size_t size = ftell(fp);
+        fseek(fp, 0L, SEEK_SET);
+        char* buffer = (char*)malloc(size);
+        fread(buffer, 1, size, fp);
+
+        vsid = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vsid, size, &buffer, NULL);
+        glCompileShader(vsid);
+    }
+} // u3d
--- u3d/src/Shader.hpp
+++ u3d/src/Shader.hpp
@@ -1,9 +1,18 @@
 #pragma once
 
+#include <string>
+#include <cstdint>
+
 namespace u3d
 {
     class Shader
     {
-
+    public:
+        void compile(const std::string&, const std::string&);
+    
+    private:
+        uint32_t vsid;
+        uint32_t fsid;
+        uint32_t pid;
     };
 } // u3d
