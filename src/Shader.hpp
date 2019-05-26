e2bfa1e63e6123b8d92be5372e0653ec4124bd73
Patrick Roche <rochep63@gmail.com>
2019-05-25 20:53:03 -0500

    forgot to save :/

--- src/Shader.hpp
+++ src/Shader.hpp
@@ -18,17 +18,8 @@ public:
     void locateUniforms();
     void fillUniforms();
 
-    void setViewMat(glm::mat4&);
-    void setProjMat(glm::mat4&);
-    void setModelMat(glm::mat4&);
+    template <typename T>
+    void setUniform(GLuint, T&);
 
 	GLuint pid;
-
-private:
-    glm::mat4* view;
-    glm::mat4* proj;
-    glm::mat4* model;
-    GLuint proj_loc;
-    GLuint view_loc;
-    GLuint model_loc;
 };
